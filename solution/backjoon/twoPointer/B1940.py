5fd016744f9a6c9a37256aa05453a730569a54f1
choideakook <shdrnrhd113@naver.com>
2024-02-23 14:28:29 +0900

    b1940: dictionary 로 리팩토링 해결

--- solution/backjoon/twoPointer/B1940.py
+++ solution/backjoon/twoPointer/B1940.py
@@ -5,27 +5,17 @@
 
 import sys
 
-def twoSum(nums, M, N):
-    nums.sort()
-    l, r = 0, N - 1
-
-    count = 0
-    while l < r:
-        sum = nums[l] + nums[r]
-
-        if sum < M:
-            l += 1
-        elif sum > M:
-            r -= 1
-        else:
-            count += 1
-            l += 1
-            r -= 1
-    return count
+def twoSum(nums, target):
+    map = {}
+    answer = 0
+    for num in nums:
+        if target - num in map: answer += 1
+        else: map[num] = nums
+    return answer
 
 input = sys.stdin.readline
 
 N = int(input())
 M = int(input())
 nums = list(map(int, input().split()))
-print(twoSum(nums, M, N))
+print(twoSum(nums, M))
