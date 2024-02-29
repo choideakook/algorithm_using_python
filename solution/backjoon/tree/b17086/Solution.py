62b3a32073702102877e39b326f80d0ed4249be8
choideakook <shdrnrhd113@naver.com>
2024-02-29 13:43:11 +0900

    b17086: 리팩토링

--- solution/backjoon/tree/b17086/Solution.py
+++ solution/backjoon/tree/b17086/Solution.py
@@ -7,38 +7,30 @@ from collections import deque
 import sys
 input = sys.stdin.readline
 
-def bfs(index):
-    visited = [[False] * M for _ in range(N)]
-    visited[index[0]][index[1]] = True
-    Q = deque()
-    Q.append((index, 1))
-
-    while Q:
-        now, count = Q.popleft()
-
-        for i in range(8):
-            Y = now[0] + dy[i]
-            X = now[1] + dx[i]
-
-            if Y >= 0 and X >= 0 and Y < N and X < M:
-                if table[Y][X] == 1:
-                    return count
-                elif not visited[Y][X]:
-                    visited[Y][X] = True
-                    Q.append(([Y, X], count + 1))
-
-    return count
-
 dy = [1, 0, -1, 0, 1, 1, -1, -1]
 dx = [0, 1, 0, -1, 1, -1, 1, -1]
 N, M = map(int, input().split())
-table = []
-for i in range(N):
-    table.append(list(map(int, input().split())))
+
+Q = deque()
+table = [[0] * M for _ in range(N)]
+for y in range(N):
+    arr = list(map(int, input().split()))
+    for x in range(M):
+        if arr[x] == 1:
+            table[y][x] = 1
+            Q.append([y, x])
 
 answer = 0
-for y in range(N):
-    for x in range(M):
-        if table[y][x] == 0:
-            answer = max(bfs([y, x]), answer)
-print(answer)
+while Q:
+    now = Q.popleft()
+    answer = max(table[now[0]][now[1]], answer)
+
+    for i in range(8):
+        Y = now[0] + dy[i]
+        X = now[1] + dx[i]
+
+        if Y >= 0 and X >= 0 and Y < N and X < M:
+            if table[Y][X] == 0:
+                table[Y][X] += table[now[0]][now[1]] + 1
+                Q.append([Y, X])
+print(answer - 1)
